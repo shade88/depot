aff4ca6aec7c519aa8717f14f015252ddf80a7e0
shade88 <shade88@yandex.ru>
2014-08-05 16:20:54 +0600

    13.1. Шаг З1: отправка подтверждающих электронных сообщений

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -10,7 +10,7 @@ class LineItemsControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:line_items)
   end
-
+                                                                                           s
   test "should get new" do
     get :new
     assert_response :success
@@ -21,7 +21,17 @@ class LineItemsControllerTest < ActionController::TestCase
       post :create, product_id: products(:ruby).id
     end
 
-    assert_redirected_to cart_path(assigns(:line_item).cart)
+    assert_redirected_to store_path
+  end
+
+  test "should create line_item via ajax" do
+    assert_difference('LineItem.count') do
+      xhr :post, :create, product_id: products(:ruby).id
+    end
+    assert_response :success
+    assert_select_jquery :html, '#cart' do
+      assert_select 'tr#current_item td', /Programming Ruby 1.9/
+    end
   end
 
   test "should show line_item" do
